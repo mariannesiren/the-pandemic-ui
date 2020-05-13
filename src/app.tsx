07c49fdcc226b850a8216a44acde5cef2f167f20
Marianne <marianne.siren@gmail.com>
2020-05-13 14:05:25 +0300

    Remove GlobalStyle

--- src/app.tsx
+++ src/app.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
 import Dashboard from './components/dashboard';
 
-const GlobalStyle = createGlobalStyle`
-    body {
-      margin: 0;
-      font-family: 'Ubuntu', sans-serif;
-      font-weight: 400;
-      background: #ebeeff;
-      padding: 2rem;
-    }
-`;
-
 const App = () => {
-
   return (
     <>
       <Dashboard />
-      {/*<GlobalStyle />*/}
     </>
   );
 };
