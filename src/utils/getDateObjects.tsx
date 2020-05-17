1250ae1ba5278877ae052b4c2c5a581e1c928afd
Marianne <marianne.siren@gmail.com>
2020-05-17 19:39:42 +0300

    Simplify function

--- src/utils/getDateObjects.tsx
+++ src/utils/getDateObjects.tsx
@@ -3,39 +3,29 @@ export const getDateObjects = (
     active: number;
     confirmed: number;
     country: string;
-    date: string;
+    date: Date;
     deaths: number;
     recovered: number;
   }[],
   endDate: Date
 ) => {
-  const year = endDate.getFullYear();
-  const month = endDate.getMonth();
-  const day = endDate.getDate();
-  const formattedMonth = month < 10 ? `0${month + 1}` : `${month + 1}`;
-  const formattedDay = day < 10 ? `0${day}` : `${day}`;
-
-  const parsedDate = `${year}-${formattedMonth}-${formattedDay}`;
-  console.log('parsed date', parsedDate);
-
-  const countriesByEndDate = coronaData.filter((data) =>
-    data.date.includes(parsedDate)
-  );
+  const countriesByEndDate = coronaData.filter((country) => {
+    const dateObject = new Date(country.date);
+    return dateObject.toDateString() === endDate.toDateString();
+  });
 
   let activeSum = 0;
   let confirmedSum = 0;
   let recoveredSum = 0;
   let deadSum = 0;
+
   countriesByEndDate.forEach((country) => {
     activeSum += country.active;
     confirmedSum += country.confirmed;
     recoveredSum += country.recovered;
     deadSum += country.deaths;
   });
-  console.log('activesum', activeSum);
-  console.log('confirmedsum', confirmedSum);
-  console.log('recoveredsum', recoveredSum);
-  console.log('deadsum', deadSum);
+
   return [
     {
       name: 'Active',
